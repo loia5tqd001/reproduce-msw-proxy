18a9a73a3476836a9cc3d1d7faca6bce09feb034
Nguyễn Huỳnh Lợi <loia5tqd001@gmail.com>
2021-09-08 23:45:38 +0700

    Setup proxy for msw

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,6 +1,18 @@
-import { setupWorker, rest, RESTMethods, ResponseResolver } from 'msw';
+import {
+  setupWorker,
+  rest,
+  RESTMethods,
+  ResponseResolver,
+  RestRequest,
+  RestContext,
+} from 'msw';
+import { headersToObject } from 'headers-utils';
 
-function createProxy(url: string, resolver: ResponseResolver) {
+function createProxy(
+  url: string,
+  resolver: ResponseResolver<RestRequest, RestContext>
+) {
+  // TODO: replace with rest.all() in the newest version
   return Object.values(RESTMethods).map((method) => {
     return rest[method.toLowerCase() as Lowercase<typeof method>](
       url,
@@ -9,16 +21,19 @@ function createProxy(url: string, resolver: ResponseResolver) {
   });
 }
 
-export const handlers = [
-  ...createProxy('/api/*', async (req, res, ctx) => {
-    const proxy = await ctx.fetch(req);
-    // How to proxy all /api to https://reqres.in/api
+const makeProxy = (fromUrl: string, toUrl: string) => {
+  return createProxy(fromUrl, async (req, res, ctx) => {
+    const proxyUrl = new URL(req.url.pathname, toUrl);
+    const proxy = await ctx.fetch(proxyUrl.href);
+
     return res(
-      ctx.status(proxy.status)
-      // ctx.set(proxy.headers as any),
-      // ctx.body(await proxy.text())
+      ctx.status(proxy.status),
+      ctx.set(headersToObject(proxy.headers)),
+      ctx.body(await proxy.text())
     );
-  }),
-];
+  });
+};
+
+export const handlers = [...makeProxy('/api/*', 'https://reqres.in')];
 
 export const worker = setupWorker(...handlers);
